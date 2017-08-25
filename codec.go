9f22559de6b57fd38212a993b7ab26613eb46c01
irfan sharif <irfanmahmoudsharif@gmail.com>
2017-08-25 19:19:13 -0400

    grpc: reduce allocations on Codec.{Unm,M}arshal codepaths
    The previous use of proto.Buffer was slightly incorrect. proto.Buffer is
    only useful when {un,}marshalling proto messages into and from the same
    buffer. The current grpc.Codec interface does not have enough
    specificity to make good use of this pattern.
    
    In the previous usage we retrieved a cachedProtoBuffer/proto.Buffer from
    a sync.Pool pool for every {Unm,M}arshal call, initialized/allocated/set
    a completely new buffer and {un,}marshalled into and from it. There was
    effectively no re-use of an allocated buffer across invocations of
    {Unm,M}arshal. Moreover by simply having used proto.Buffer as an
    intermediary between all proto.{Unm,M}arshal calls, we ended up
    effectively allocating 2x bytes when only 1x was sufficient. This is due
    to the internal implementation details of proto.Buffer where for every
    proto.(Marshaler).Marshal() call that returned a byte slice,
    proto.Buffer copies it over into it's internal buffer (which we
    explicitly initialize/set ourselves).

--- codec.go
+++ codec.go
@@ -19,9 +19,6 @@
 package grpc
 
 import (
-	"math"
-	"sync"
-
 	"github.com/golang/protobuf/proto"
 )
 
@@ -39,66 +36,16 @@ type Codec interface {
 }
 
 // protoCodec is a Codec implementation with protobuf. It is the default codec for gRPC.
-type protoCodec struct {
-}
-
-type cachedProtoBuffer struct {
-	lastMarshaledSize uint32
-	proto.Buffer
-}
-
-func capToMaxInt32(val int) uint32 {
-	if val > math.MaxInt32 {
-		return uint32(math.MaxInt32)
-	}
-	return uint32(val)
-}
-
-func (p protoCodec) marshal(v interface{}, cb *cachedProtoBuffer) ([]byte, error) {
-	protoMsg := v.(proto.Message)
-	newSlice := make([]byte, 0, cb.lastMarshaledSize)
-
-	cb.SetBuf(newSlice)
-	cb.Reset()
-	if err := cb.Marshal(protoMsg); err != nil {
-		return nil, err
-	}
-	out := cb.Bytes()
-	cb.lastMarshaledSize = capToMaxInt32(len(out))
-	return out, nil
-}
+type protoCodec struct{}
 
 func (p protoCodec) Marshal(v interface{}) ([]byte, error) {
-	cb := protoBufferPool.Get().(*cachedProtoBuffer)
-	out, err := p.marshal(v, cb)
-
-	// put back buffer and lose the ref to the slice
-	cb.SetBuf(nil)
-	protoBufferPool.Put(cb)
-	return out, err
+	return proto.Marshal(v.(proto.Message))
 }
 
 func (p protoCodec) Unmarshal(data []byte, v interface{}) error {
-	cb := protoBufferPool.Get().(*cachedProtoBuffer)
-	cb.SetBuf(data)
-	v.(proto.Message).Reset()
-	err := cb.Unmarshal(v.(proto.Message))
-	cb.SetBuf(nil)
-	protoBufferPool.Put(cb)
-	return err
+	return proto.Unmarshal(data, v.(proto.Message))
 }
 
 func (protoCodec) String() string {
 	return "proto"
 }
-
-var (
-	protoBufferPool = &sync.Pool{
-		New: func() interface{} {
-			return &cachedProtoBuffer{
-				Buffer:            proto.Buffer{},
-				lastMarshaledSize: 16,
-			}
-		},
-	}
-)
